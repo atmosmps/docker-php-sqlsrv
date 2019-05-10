50ab5c5b6e6b01317127ab7f67e63055389abac3
Atmos Maciel <atmos.mps@gmail.com>
2019-05-10 15:27:45 -0300

    atualizando estrutura do index.php que é o arquivo de teste da conexão.

--- web-samples/public/index.php
+++ web-samples/public/index.php
@@ -1,47 +1,47 @@
 <?php
-    $serverName = 'servername.com.br, port';
-    $connectionOptions = array(
-        'database' => 'DATABASE_NAME',
-        'uid' => 'username',
-        'pwd' => 'password'
-    );
+$serverName = 'servername.com.br, port';
+$connectionOptions = array(
+    'database' => 'DATABASE_NAME',
+    'uid' => 'username',
+    'pwd' => 'password'
+);
 
-    // Establishes the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    if ($conn === false) {
-        die(formatErrors(sqlsrv_errors()));
-    }
+// Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if ($conn === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
 
-    // Select Query
-    $tsql = 'select @@version as version;';
+// Select Query
+$tsql = 'select @@version as version;';
 
-    // Executes the query
-    $stmt = sqlsrv_query($conn, $tsql);
+// Executes the query
+$stmt = sqlsrv_query($conn, $tsql);
 
-    // Error handling
-    if ($stmt === false) {
-        die(formatErrors(sqlsrv_errors()));
-    }
+// Error handling
+if ($stmt === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
 ?>
 
 <h2>Connection established with Sql Server!</h2>
 <h3>Sql Server Version:</h3>
 
 <?php
-    $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-    echo $row['version'] . PHP_EOL;
+$row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+echo $row['version'] . PHP_EOL;
 
-    sqlsrv_free_stmt($stmt);
-    sqlsrv_close($conn);
+sqlsrv_free_stmt($stmt);
+sqlsrv_close($conn);
 
-    function formatErrors($errors)
-    {
-        // Display errors
-        echo 'Error information: <br/>';
-        foreach ($errors as $error) {
-            echo 'SQLSTATE: ' . $error['SQLSTATE'] . '<br/>';
-            echo 'Code: ' . $error['code'] . '<br/>';
-            echo 'Message: ' . $error['message'] . '<br/>';
-        }
+function formatErrors($errors)
+{
+    // Display errors
+    echo 'Error information: <br/>';
+    foreach ($errors as $error) {
+        echo 'SQLSTATE: ' . $error['SQLSTATE'] . '<br/>';
+        echo 'Code: ' . $error['code'] . '<br/>';
+        echo 'Message: ' . $error['message'] . '<br/>';
     }
+}
 ?>
