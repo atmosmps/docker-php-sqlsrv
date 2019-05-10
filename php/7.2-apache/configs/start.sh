7f744d388fbd270edb7ad739931cf9dbd18d1c64
Atmos Maciel <atmos.mps@gmail.com>
2019-05-10 18:18:24 -0300

    Adicionando arquivo de entrypoint start.sh.

---
+++ php/7.2-apache/configs/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# fix home directory permissions.
+sudo chown -R devceuma:devceuma /home/devceuma
+
+# copy bash config into place.
+cp /home/bashrc /home/devceuma/.bashrc
+
+# Set PHP memory limit value.
+sudo sed -i "/memory_limit = .*/c\memory_limit = $PHP_MEMORY_LIMIT" /etc/php7/php.ini
