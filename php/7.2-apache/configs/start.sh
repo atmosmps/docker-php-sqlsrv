2b9d786515c65f6e04a82a99bf0085185626960d
Atmos Maciel <atmos.mps@gmail.com>
2019-05-11 18:56:57 -0300

    Removendo configuracao para alterar o memory_limit do PHP.

--- php/7.2-apache/configs/start.sh
+++ php/7.2-apache/configs/start.sh
@@ -6,5 +6,5 @@ sudo chown -R devceuma:devceuma /home/devceuma
 # copy bash config into place.
 cp /home/bashrc /home/devceuma/.bashrc
 
-# Set PHP memory limit value.
-sudo sed -i "/memory_limit = .*/c\memory_limit = $PHP_MEMORY_LIMIT" /etc/php7/php.ini
+## Set PHP memory limit value.
+#sudo sed -i "/memory_limit = .*/c\memory_limit = $PHP_MEMORY_LIMIT" /etc/php7/php.ini
